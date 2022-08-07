06f8cb018622653a424c4051e5a24d25692d9ede
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-08-07 14:02:29 -0600

    descuentos

--- src/descuentos.js
+++ src/descuentos.js
@@ -1,23 +1,47 @@
 // Nodos
 const btn = document.querySelector('#calcular');
 const inputPrice = document.querySelector('#price');
-const inputDiscount = document.querySelector('#discount');
+const inputCoupon = document.querySelector('#coupon');
 const pResult = document.querySelector('#result')
 
 // eventos
 btn.addEventListener('click', calcularPrecioConDescuento);
 
+// constantes
+const coupons = {
+	'TEXTO_DEL_CUPON20': {
+		valid: true,
+		value: 20,
+	},
+	'TEXTO_DEL_CUPON10': {
+		valid: true,
+		value: 10,
+	},
+}
+
+// funciones 
 function calcularPrecioConDescuento() {
 	/*
 	Formula: (P * (100 - D) / 100)
 	*/
 	const price = Number(inputPrice.value);
-	const discount = Number(inputDiscount.value);
+	const coupon = inputCoupon.value;
+	let discount;
 
-	if (!price || !discount) {
+	if (!price || !coupon) {
 		pResult.innerText = "Por favor llena los campos."
 		return;
 	}
+
+	coupon_valid = coupons[coupon];
+	console.log(coupon)
+
+	if(!coupon_valid || !coupon_valid.valid) {
+		pResult.innerText = `Lo sentimos 😭, el cupon: ${coupon_valid}, no es valido`
+		return;
+	}
+
+	discount = coupon_valid.value;
 
 	if (discount > 100) {
 		pResult.innerText = `${discount}!! DE DESCUENTO 👀. Creo que escribieste mal.`
