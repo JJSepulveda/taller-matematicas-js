7e8717d0db53ef322efdda142d9d9dfa6d72f9f2
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-08-10 14:20:43 -0600

    clase21_proyeccion_salarial

--- src/analisis.js
+++ src/analisis.js
@@ -1,6 +1,10 @@
 console.log(salarios)
 
-// Analiss personal
+/**
+ * Find a person an return its data
+ * @param {String} personaEnBusqueda - The name of the person
+ * @returns {Object} persona - The information of the person
+ * */
 function encontrarPersona(personaEnBusqueda) {
 	// como no se coloco un idendificador en el arreglo salarios
 	// se esta usando el nombre de la persona
@@ -12,6 +16,11 @@ function encontrarPersona(personaEnBusqueda) {
 }
 
 
+/**
+ * Calculate the median of the salary of one person
+ * @param {String} nombrePersona - The name of the person
+ * @returns {Number} medianaPorPersona - The median of the salaries
+ * */
 function medianaDeSalariosPorPersona(nombrePersona) {
 	const trabajos = encontrarPersona(nombrePersona).trabajos;
 
@@ -22,4 +31,36 @@ function medianaDeSalariosPorPersona(nombrePersona) {
 	const medianaSalarios = PlatziMath.calcularMediana(salarios);
 
 	return medianaPorPersona;
+}
+
+/**
+ * Calculate salary's percentage growth and returns the new salary.
+ * @param {String} nombrePersona - The name of the person
+ * @returns {Number} nuevoSalario - The new salary based on salary's percentage growth 
+ * */
+function porcentajeDeCrecimiento(nombrePersona) {
+	const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+	let porcentajesCrecimiento = [];
+
+	for (let i = 1; i < trabajos.length; i++) {
+		const salarioActual = trabajos[i].salario;
+		const salarioPasado = trabajos[i - 1].salario;
+		const crecimiento = salarioActual - salarioPasado;
+		const porcentajeCrecimiento = crecimiento / salarioPasado;
+		porcentajesCrecimiento.push(porcentajeCrecimiento)
+	}
+
+	const medianaDePorcentajeDeCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+	const ultimoSalario = trabajos[trabajos.length - 1].salario;
+	const aumento = ultimoSalario * medianaDePorcentajeDeCrecimiento;
+	const nuevoSalario = ultimoSalario + aumento
+
+	console.log({
+		porcentajesCrecimiento,
+		medianaDePorcentajeDeCrecimiento,
+		nuevoSalario
+	})
+
+	return nuevoSalario;
 }
